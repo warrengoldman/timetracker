c5cdf22e8d17e088500ca23c860ab993523bfdef
Warren Goldman <Warren.Goldman@pacelabs.com>
2021-04-26 18:07:30 -0500

    Added stuff to make app work

--- src/main/java/com/goldman/timetracker/TimetrackerApplication.java
+++ src/main/java/com/goldman/timetracker/TimetrackerApplication.java
@@ -2,12 +2,18 @@ package com.goldman.timetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TimetrackerApplication {
+public class TimetrackerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimetrackerApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TimetrackerApplication.class);
+	}
 }
